bd089446d351b65881d64eaaa7f3849356ed4605
anin21-taq <anindrataqi1@gmail.com>
2026-06-30 15:25:50 +0700

    update

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@
 
     current = index;
 
-    const duration = parseInt(slides[index].dataset.duration, 10);
+    const duration = parseInt(slides[index].dataset.duration, 15);
     if(duration > 0){
       const seg = segFills[index];
       const fill = seg.querySelector('.progress-seg-fill');
